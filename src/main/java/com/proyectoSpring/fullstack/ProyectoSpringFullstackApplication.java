07cf30d4ceb0913799c88c8cde749f5f3808daaf
javier-tc <javier.tapia.cvl@gmail.com>
2025-04-23 16:26:29 -0400

    Commit inicial...

---
+++ src/main/java/com/proyectoSpring/fullstack/ProyectoSpringFullstackApplication.java
@@ -0,0 +1,13 @@
+package com.proyectoSpring.fullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoSpringFullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoSpringFullstackApplication.class, args);
+	}
+
+}
